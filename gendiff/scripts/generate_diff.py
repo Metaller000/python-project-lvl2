906f01fd09c6a61c6bdc24688c5e9878e12f1934
Metaller <metaller7440326@gmail.com>
2021-03-08 21:08:10 +0300

    remove file_load test; added step 6

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -3,15 +3,12 @@ import json
 import yaml
 
 
-def get_str(val):
-    if val is False:
-        return 'false'
-    elif val is True:
-        return 'true'
-    elif val is None:
-        return 'null'
-    else:
-        return val
+def data_sort(data):
+    json_data = json.dumps(data, sort_keys=True)
+    json_data = json_data.replace('null', '"null"')
+    json_data = json_data.replace('true', '"true"')
+    json_data = json_data.replace('false', '"false"')
+    return json.loads(json_data)
 
 
 def get_dict_from_file(file):
@@ -19,34 +16,64 @@ def get_dict_from_file(file):
     if '.json' in file:
         with open(file) as f:
             data = json.load(f)
-            data = json.loads(json.dumps(data, sort_keys=True))
     if '.yml' in file:
         with open(file) as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
-            data = yaml.load(yaml.dump(data), Loader=yaml.FullLoader)
     return data
 
 
-def generate_diff(file_1, file_2):
-    dict_1 = get_dict_from_file(file_1)
-    dict_2 = get_dict_from_file(file_2)
+def diff(data_1, data_2):
+    data = {}
+    for res in data_1:
+        if res in data_2:
+            res_1 = data_1[res]
+            res_2 = data_2[res]
+            if type(res_1) is dict:
+                if type(res_2) is not dict:
+                    data.update({f'{res}:1': res_1})
+                    data.update({f'{res}:2': res_2})
+                else:
+                    data.update({f'{res}': diff(res_1, res_2)})
+            else:
+                if res_1 == res_2:
+                    data.update({f'{res}': res_1})
+                else:
+                    data.update({f'{res}:1': res_1})
+                    data.update({f'{res}:2': res_2})
+            del data_2[res]
+        else:
+            data.update({f'{res}:1': data_1[res]})
+
+    for res in data_2:
+        data.update({f'{res}:2': data_2[res]})
+
+    return data
+
+
+def stylish(data={}, spaces_num=0):
+    spaces = spaces_num * ' '
 
     output = '{'
-    for res in dict_1:
-        if res in dict_2:
-            res_1 = get_str(dict_1[res])
-            res_2 = get_str(dict_2[res])
-            if res_1 == res_2:
-                output = f'{output}\n    {res}: {res_1}'
-            else:
-                output = f'{output}\n  - {res}: {res_1}'
-                output = f'{output}\n  + {res}: {res_2}'
-            del dict_2[res]
+    for key, value in data.items():
+        if key[-2:] == ':1':
+            key_out = f'  - {key[:-2]}'
+        elif key[-2:] == ':2':
+            key_out = f'  + {key[:-2]}'
         else:
-            output = f'{output}\n  - {res}: {get_str(dict_1[res])}'
+            key_out = f'    {key}'
 
-    for res in dict_2:
-        output = f'{output}\n  + {res}: {get_str(dict_2[res])}'
+        if type(value) is dict:
+            values = stylish(value, spaces_num + 4)
+            output = f'{output}\n{spaces}{key_out}: {values}'
+        else:
+            output = f'{output}\n{spaces}{key_out}: {value}'
+    output = f'{output}\n{spaces}' + '}'
 
-    output = f'{output}' + '\n}'
     return output
+
+
+def generate_diff(file_1, file_2):
+    data = diff(get_dict_from_file(file_1), get_dict_from_file(file_2))
+    data = data_sort(data)
+
+    return stylish(data)
---
+++ tests/test_generate_diff.py
@@ -0,0 +1,66 @@
+import pytest
+import os
+from gendiff.scripts.generate_diff import generate_diff
+
+full_path = f'{os.path.dirname(os.path.abspath(__file__))}/fixtures'
+
+
+def test_json_generate_diff_1():
+    data_1 = ''
+    with open(f'{full_path}/result_1') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_1.json',
+                           f'{full_path}/file_2.json')
+
+    assert data_2 == data_1
+
+
+def test_json_generate_diff_2():
+    data_1 = ''
+    with open(f'{full_path}/result_2') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_3.json',
+                           f'{full_path}/file_4.json')
+    assert data_2 == data_1
+
+
+def test_yaml_generate_diff_1():
+    data_1 = ''
+    with open(f'{full_path}/result_1') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_1.yml',
+                           f'{full_path}/file_2.yml')
+    assert data_2 == data_1
+
+
+def test_yaml_generate_diff_2():
+    data_1 = ''
+    with open(f'{full_path}/result_2') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_3.yml',
+                           f'{full_path}/file_4.yml')
+    assert data_2 == data_1
+
+
+def test_json_yaml_generate_diff_1():
+    data_1 = ''
+    with open(f'{full_path}/result_1') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_1.json',
+                           f'{full_path}/file_2.yml')
+    assert data_2 == data_1
+
+
+def test_json_yaml_generate_diff_2():
+    data_1 = ''
+    with open(f'{full_path}/result_2') as file:
+        data_1 = file.read()
+
+    data_2 = generate_diff(f'{full_path}/file_3.json',
+                           f'{full_path}/file_4.yml')
+    assert data_2 == data_1
